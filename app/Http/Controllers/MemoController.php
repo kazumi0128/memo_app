c9240c3ce65338f34d6d869514aa7ab0b8887857
kazumi0128 <sh000128sh@gmail.com>
2025-01-19 15:38:46 +0900

    練習完了

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -1,27 +1,42 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Memo;
 use Illuminate\Http\Request;
 
 class MemoController extends Controller
 
 {
-public function index()
-{
-    //全件取得
-    $memos = Memo::all();
-    return view('memos.index', ['memos' => $memos]);
-}
+    public function index()
+    {
+        //全件取得
+        $memos = Memo::all();
+        return view('memos.index', ['memos' => $memos]);
+    }
+
+    public function create()
+    {
+        return view('memos.create');
+    }
+
+    public function store(Request $request)
+    {
+        // インスタンスの作成
+        $memo = new Memo();
+
+        $memo->title = $request->title;
+        $memo->body = $request->body;
+        $memo->save();
+
+        // 登録したらindexに戻る
+        return redirect(route('memos.index'));
+    }
 
 
-
-
-public function show($id)
-{
-$memo = Memo::find($id);
-return view('memos.show', ['memo' => $memo]);
-
-}
-
+    public function show($id)
+    {
+        $memo = Memo::find($id);
+        return view('memos.show', ['memo' => $memo]);
+    }
 }
