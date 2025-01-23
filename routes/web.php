c9646f79c6e5c167c13b91cda9fcaa7fb6370ac0
kazumi0128 <sh000128sh@gmail.com>
2025-01-23 10:44:05 +0900

    デプロイ

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\MemoController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[MemoController::class,'inedex']);
 
 Route::resource('memos', MemoController::class);
